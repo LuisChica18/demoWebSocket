d8cf4be208064e80b03dadd7fc14b6647320bf7b
Luis Chica Moncayo <luischicamoncayo@hotmail.com>
2022-11-23 22:38:17 -0500

    Implements Spring-websockets for chat application

---
+++ src/main/java/com/example/demowebsocket/controlador/BotsController.java
@@ -0,0 +1,23 @@
+package com.example.demowebsocket.controlador;
+
+import com.example.demowebsocket.modelo.Message;
+import com.example.demowebsocket.modelo.OutputMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class BotsController {
+
+    @MessageMapping("/chatwithbots")
+    @SendTo("/topic/pushmessages")
+    public OutputMessage send(final Message message) throws Exception {
+
+        final String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage(message.getFrom(), message.getText(), time);
+    }
+
+}
---
+++ src/test/java/com/example/demowebsocket/DemoWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demowebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
